c820d349e24fa1731655dc951680ce40e3247a26
Kunwoo Yoon <ykw0628@gmail.com>
2020-10-01 12:54:36 -0400

    contact, about me

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,26 @@
+'use strict';
 // IIFF --Immediately Invoked Function Expression
 
 (function(){
 
     function Start() {
         console.log("App Started...");
+    // Make navbar transparent when it is on the top
+    const navbar = document.querySelector('#navbar');
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > navbarHeight) {
+            navbar.classList.add('navbar--dark');
+        }else{
+            navbar.classList.remove('navbar--dark');
+        }
+    });
+
+    //Navbar toggle button for small screen
+    const navbarToggle = document.querySelector('.navbar__toggle-btn');
+    navbarToggle.addEventListener('click', () =>{
+        navbarMenu.classList.toggle('open');
+    });
     }
     window.addEventListener("load", Start);
 })();
--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,27 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('Request for home recieved');
     res.render('index');
   });
   router.get('/index', (req, res) => {
-    console.log('Request for Index recieved');
     res.render('index');
   });
   router.get('/aboutMe', (req, res) => {
-    console.log('Request for About me recieved');
-    res.render('index');
-  });
-  router.get('/projects', (req, res, next) => {
-    res.render('index', {title: 'projects'});
-  });
-  router.get('/services', (req, res, next) => {
-    res.render('index', {title: 'services'});
+    res.render('aboutMe');
   });
 
-  router.get('/contact', (req, res, next) => {
-    res.render('index', {title: 'contact'});
+  router.get('/projects', (req, res) => {
+    res.render('index');
   });
+
+  router.get('/services', (req, res) => {
+    res.render('index');
+  });
+
+  router.get('/contact', (req, res) => {
+    res.render('contact');
+  });
+
+
 
 module.exports = router;
